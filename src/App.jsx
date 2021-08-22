59ffdde03bfd3ae6385a74967c709c55efa4f0ab
sulmo <tjdah0853@gmail.com>
2021-08-23 04:02:01 +0900

    Step 3,4 : fiber 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// 엘리먼트가 아닌 primitive value에대한 wrapping 함수
 function createTextElement(text) {
   return {
     type: "TEXT_ELEMENT",
@@ -8,6 +9,7 @@ function createTextElement(text) {
   };
 }
 
+// 돔요소 생성
 function createElement(type, props, ...children) {
   return {
     type,
@@ -20,22 +22,85 @@ function createElement(type, props, ...children) {
   };
 }
 
-function render(element, container) {
+// fiber 자료구조를 이용한 DOM 생성
+function createDom(fiber) {
   const dom =
-    element.type == "TEXT_ELEMENT"
+    fiber.type == "TEXT_ELEMENT"
       ? document.createTextNode("")
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
+
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
+
+  Object.keys(fiber.props)
     .filter(isProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
-  element.props.children.forEach((child) => render(child, dom));
-  container.appendChild(dom);
+
+  return dom;
 }
 
-const Didact = {
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+  while (index < elements.length) {
+    const element = elements[index];
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
+}
+
+export const Didact = {
   createElement,
   render,
 };
