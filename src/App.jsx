f4d779bed230b6bbc9872ee756749b72a73a2b5c
sulmo <tjdah0853@gmail.com>
2021-09-08 21:51:44 +0900

    step 6: reconciliation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 // 엘리먼트가 아닌 primitive value에대한 wrapping 함수
 function createTextElement(text) {
   return {
@@ -29,19 +30,70 @@ function createDom(fiber) {
       ? document.createTextNode("")
       : document.createElement(fiber.type);
 
-  const isProperty = (key) => key !== "children";
-
-  Object.keys(fiber.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = fiber.props[name];
-    });
+  updateDom(dom, {}, fiber.props) 
 
   return dom;
 }
 
+// updateDom에서 사용할 메소드
+const isEvent = key => key.startsWith("on") // 이벤트 리스너 분리
+const isProperty = key => key !== "children" && !isEvent(key) 
+const isNew = (prev, next) => key => prev[key] !== next[key]
+const isGone = (prev, next) => key => !(key in next)
+
+// 추가와 삭제는 appendChild, removeChild 함수를 쓰면 되지만 업데이트는 프롭스를 변경시켜줘야 한다.
+function updateDom(dom, prevProps, nextProps) {
+  // 안쓰거나 수정된 이벤트 리스너 제거
+  Object.keys(prevProps)
+    .filter(isEvent)
+    .filter(
+      key =>
+        !(key in nextProps) ||
+        isNew(prevProps, nextProps)(key)
+    )
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.removeEventListener(
+        eventType,
+        prevProps[name]
+      )
+    })
+  // 필요 없어진 props 제거
+  Object.keys(prevProps)
+    .filter(isProperty)
+    .filter(isGone(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = ""
+    })
+  // 새로 생성되거나 수정된 props 적용
+  Object.keys(nextProps)
+    .filter(isProperty)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      dom[name] = nextProps[name]
+    })
+  
+  // 이벤트 리스너 추가 로직
+  Object.keys(nextProps)
+    .filter(isEvent)
+    .filter(isNew(prevProps, nextProps))
+    .forEach(name => {
+      const eventType = name
+        .toLowerCase()
+        .substring(2)
+      dom.addEventListener(
+        eventType,
+        nextProps[name]
+      )
+    })
+}
+
 function commitRoot() {
+  deletions.forEach(commitWork); 
   commitWork(wipRoot.child);
+  currentRoot = wipRoot // 최신 파이버트리를 실제 Dom트리에 커밋 완료 후 따로 저장
   wipRoot = null;
 }
 
@@ -50,7 +102,25 @@ function commitWork(fiber) {
     return;
   }
   const domParent = fiber.parent.dom;
-  domParent.appendChild(fiber.dom);
+  // effectTag별 처리
+  // PLACEMENT == 생성, UPDATE == 수정, DELETION == 삭제
+  if (
+    fiber.effectTag === "PLACEMENT" &&
+    fiber.dom != null
+  ) {
+    domParent.appendChild(fiber.dom)
+  } else if (
+    fiber.effectTag === "UPDATE" &&
+    fiber.dom != null
+  ) {
+    updateDom(
+      fiber.dom,
+      fiber.alternate.props,
+      fiber.props
+    )
+  } else if (fiber.effectTag === "DELETION") {
+    domParent.removeChild(fiber.dom)
+  }
   commitWork(fiber.child);
   commitWork(fiber.sibling);
 }
@@ -61,12 +131,16 @@ function render(element, container) {
     props: {
       children: [element],
     },
+    alternate: currentRoot, // 이전 커밋단계의 파이버 트리와 연결을 위함
   };
+  deletions = []
   nextUnitOfWork = wipRoot;
 }
 
 let nextUnitOfWork = null;
+let currentRoot = null;
 let wipRoot = null;
+let deletions = null;
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -83,32 +157,13 @@ function workLoop(deadline) {
 }
 requestIdleCallback(workLoop);
 
+// 작업 단위별 수행 기능에서 새로운 파이버 생성 로직 분리
 function performUnitOfWork(fiber) {
   if (!fiber.dom) {
     fiber.dom = createDom(fiber);
   }
-  const elements = fiber.props.children;
-  let index = 0;
-  let prevSibling = null;
-
-  // 자식 파이버 생성 및 연결
-  while (index < elements.length) {
-    const element = elements[index];
-    const newFiber = {
-      type: element.type,
-      props: element.props,
-      parent: fiber,
-      dom: null,
-    };
-
-    if (index === 0) {
-      fiber.child = newFiber;
-    } else {
-      prevSibling.sibling = newFiber;
-    }
-    prevSibling = newFiber;
-    index++;
-  }
+  const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
 
   if (fiber.child) {
     return fiber.child;
@@ -122,10 +177,63 @@ function performUnitOfWork(fiber) {
   }
 }
 
+function reconcileChildren(wipFiber, elements){
+  let index = 0;
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child;
+  let prevSibling = null;
+
+  // 자식 파이버 생성 및 연결
+  while (index < elements.length || oldFiber != null) {
+    const element = elements[index];
+    let newFiber = null;
+    const sameType = oldFiber && element && element.type == oldFiber.type
+
+    if (sameType) {
+      // Todo update the node
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: "UPDATE",
+      }
+    }
+    if (element && !sameType){
+      // Todo add this node
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: "PLACEMENT",
+      }
+    }
+    if (oldFiber && !sameType){
+      // Todo delete the oldFiber's node
+      oldFiber.effectTag = "DELETION"
+      deletions.push(oldFiber)
+    }
+    if (oldFiber) {
+        oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+    prevSibling = newFiber;
+    index++;
+  }
+}
+
 export const Didact = {
   createElement,
   render,
 };
+
 /** @jsx Didact.createElement */
 const element = (
   <div id="foo">
